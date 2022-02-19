1f319404af0ecd310e81d4c74a980ad8edd6681e
k.donahue45@yahoo.com <k.donahue45@yahoo.com>
2022-02-19 18:27:12 -0500

    restructured intial file setup

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import Nav from './Nav';
+
+function Footer() {
+return (
+    <header className='footer'>
+        <Nav />
+    </header>
+);
+}
+
+export default Footer;
