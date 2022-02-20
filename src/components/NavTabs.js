02e93622434e030bb8cb89a6b980232092f91649
k.donahue45@yahoo.com <k.donahue45@yahoo.com>
2022-02-19 22:26:29 -0500

    edits to components and added headshot

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -33,7 +33,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
         >
-          Blog
+          Projects
         </a>
       </li>
       <li className="nav-item">
